7774cab13a1ad54cfad5868822ca409182267c0b
Chris Ford <christophertford@gmail.com>
2012-11-13 15:18:07 +0530

    Refactored bell to reduce nesting.

--- src/goldberg/variations/canone_alla_quarta.clj
+++ src/goldberg/variations/canone_alla_quarta.clj
@@ -51,18 +51,17 @@
 
 (definst bell [frequency 440 duration 10
                h0 1 h1 0.6 h2 0.4 h3 0.25 h4 0.2 h5 0.15]
-  (let [harmonics   [ 1  2  3  4  5 6 ]
-        proportions [h0 h1 h2 h3 h4 h5]
-        proportional-partial
+
+  (let [harmonic-series [ 1  2  3  4  5  6]
+        proportions     [h0 h1 h2 h3 h4 h5]
+        component
          (fn [harmonic proportion]
-           (let [envelope
-                  (env-gen (perc 0.01 (* proportion duration)))
-                 overtone
-                  (* harmonic frequency)]
-             (* 1/2 proportion envelope (sin-osc overtone))))
-        partials
-          (map proportional-partial harmonics proportions)
-        whole (mix partials)]
+           (* 1/2
+              proportion
+              (env-gen (perc 0.01 (* proportion duration)))
+              (sin-osc (* harmonic frequency))))
+        whole
+          (mix (map component harmonic-series proportions))]
     (detect-silence whole :action FREE)
     whole))
 
