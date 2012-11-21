3897b07a72afc64dd47f3724930b2904b276fceb
Chris Ford <christophertford@gmail.com>
2012-11-21 21:34:10 +0530

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject goldberg "0.1.0-SNAPSHOT"
   :description "The Goldberg Variations in Overtone."
   :dependencies	[
-    [org.clojure/clojure "1.3.0"]
-    [overtone "0.7.1"]
+    [org.clojure/clojure "1.4.0"]
+    [overtone "0.7.1" :exclusions [seesaw]]
     [quil "1.6.0"]
   ]
 )
