872615b859f4423faaf5384765b1e840002b71e8
Chris Ford <christophertford@gmail.com>
2017-01-19 21:34:43 +0000

    Update Overtone.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "The Goldberg Variations in Overtone."
   :dependencies	[
     [org.clojure/clojure "1.8.0"]
-    [overtone "0.9.1"]
+    [overtone "0.10.1"]
     [quil "2.5.0"]
     [leipzig "0.10.0"]])
