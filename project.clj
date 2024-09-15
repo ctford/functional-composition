e74146f15f44c097cc105d0c8779c820e1bad601
Chris Ford <christophertford@gmail.com>
2024-09-15 15:33:57 +0200

    Exclue troublesome jvm options.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject goldberg "0.1.0-SNAPSHOT"
   :description "The Goldberg Variations in Overtone."
+  :jvm-opts ^:replace []
   :dependencies	[
     [org.clojure/clojure "1.8.0"]
     [overtone "0.10.3"]
